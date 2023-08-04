082ed3b8189ff1ff6efc9ed4493c525df5035dbe
ofer <oferc008@gmail.com>
2023-08-04 13:04:23 +0300

    added postgress

--- flights_website/settings.py
+++ flights_website/settings.py
@@ -78,11 +78,18 @@ WSGI_APPLICATION = 'flights_website.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+     'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': 'ofer',
+           'USER': 'ofer_user',
+           'PASSWORD': 'uhpkiDPfAVHgJ6RGSFYnaiul09HxJDrt',
+           'HOST': 'dpg-cj6ckj17120s739v0dtg-a.oregon-postgres.render.com',  }
 }
+
 
 
 # Password validation
